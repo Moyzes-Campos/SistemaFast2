a0dc6af4b7b8105c7463eb432739f72c0abac51a
Moyzes <moyzesstaron@gmail.com>
2023-02-10 18:39:25 -0300

    Alterando o Settings

--- sistemafast/settings.py
+++ sistemafast/settings.py
@@ -106,10 +106,6 @@ MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "arquivos",
-]
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'index_dash'
