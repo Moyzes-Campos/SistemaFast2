e0940b004a9a35ed653c7c16bbdedd207b170de4
Moyzes <moyzesstaron@gmail.com>
2023-02-11 08:37:08 -0300

    Mais uma tentativa :/

--- sistemafast/settings.py
+++ sistemafast/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
