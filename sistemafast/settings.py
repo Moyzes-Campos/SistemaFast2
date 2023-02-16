3a232b6bd0b7b5ede28ec48ed2cae77bfb69885e
Moyzes <moyzesstaron@gmail.com>
2023-02-16 12:07:18 -0300

    Alterando a landing page

--- sistemafast/settings.py
+++ sistemafast/settings.py
@@ -78,10 +78,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'arquivos/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
