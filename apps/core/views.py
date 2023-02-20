2aa9940c02c530824fb2508bd12a89fcf9d1415c
Moyzes <moyzesstaron@gmail.com>
2023-02-20 16:11:46 -0300

    Atualizando o site

--- apps/core/views.py
+++ apps/core/views.py
@@ -4,3 +4,11 @@ from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = 'index.html'
+
+
+class History(TemplateView):
+    template_name = 'our_history.html'
+
+
+class Contato(TemplateView):
+    template_name = 'contato.html'
