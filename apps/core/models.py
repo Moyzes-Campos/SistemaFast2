afdcae781c300da5464ae7278f3e8c55a7725eda
Moyzes <moyzesstaron@gmail.com>
2023-02-22 11:44:12 -0300

    Adicionando a seção de patrocinadores

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from stdimage import StdImageField
 
-# Create your models here.
+
+class Patrocinador(models.Model):
+    nome = models.CharField(max_length=50)
+    logo = StdImageField('foto', upload_to='integrantes', blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Patrocinador'
+        verbose_name_plural = 'Patrocinadores'
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Patrocinador
 
 
 class Index(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Index, self).get_context_data(**kwargs)
+        context['patrocinador'] = Patrocinador.objects.all()
+        return context
 
 
 class History(TemplateView):
